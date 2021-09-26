edbd188d5112be171b5257a586dad2637ad27d71
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-09-26 12:09:44 +0800

    change env

--- srcs/index.js
+++ srcs/index.js
@@ -24,7 +24,8 @@ app.set('port', PORT);
 app.use(bodyParser.json());
 app.use((req, res, next) => {
 	//res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-	res.header("Access-Control-Allow-Origin", "https://booking-42kl.web.app");
+	//res.header("Access-Control-Allow-Origin", "https://booking-42kl.web.app");
+	res.header("Access-Control-Allow-Origin", process.env.ALLOWED_ORIGIN);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
 	next();
