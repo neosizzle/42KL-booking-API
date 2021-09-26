452fe34d0756dce182c37c76b144efebb28e5b47
neosizzle <44501267+neosizzle@users.noreply.github.com>
2021-09-26 11:53:56 +0800

    added prod for CORS

--- srcs/routes/auth.js
+++ srcs/routes/auth.js
@@ -16,7 +16,7 @@ router.post('/auth/get_token/:code', (req,res)=>
 	let	url_str;
 
 	code = req.params.code;
-	url_str = `https://api.intra.42.fr/oauth/token?grant_type=${process.env.GRANT_TYPE_42}&client_id=${process.env.API_UID_42}&client_secret=${process.env.API_SECRET_42}&code=${code}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth`;
+	url_str = `https://api.intra.42.fr/oauth/token?grant_type=${process.env.GRANT_TYPE_42}&client_id=${process.env.API_UID_42}&client_secret=${process.env.API_SECRET_42}&code=${code}&redirect_uri=${process.env.TOKEN_REDIR_URI_42}`;
 	axios.post(url_str)
 	.then((response) => 
 	{
